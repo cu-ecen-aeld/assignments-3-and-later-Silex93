fea46ce26ab05c9a47b8f5f98052176c77456faa
Daniel Mendez <silex93@gmail.com>
2023-09-04 02:20:40 -0600

    Initial commit

--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -14,9 +14,12 @@
 */
 void test_validate_my_username()
 {
-    /**
-     * TODO: Replace the line below with your code here as described above to verify your /conf/username.txt 
-     * config file and my_username() functions are setup properly
-     */
-    TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
+    //Get the harcoded username
+    const char * hardcoded_username = my_username();
+    //Get the github username
+    char * config_username = malloc_username_from_conf_file();
+    //Run the test
+    TEST_ASSERT_EQUAL_STRING_MESSAGE(hardcoded_username,config_username,"Username Validation Failed");
+    //Free the memory used by the malloc function
+    free(config_username);
 }
