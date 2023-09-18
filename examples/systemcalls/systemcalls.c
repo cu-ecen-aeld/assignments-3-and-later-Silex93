e592b64f817ba9d2121597ffa7f903fc12790044
Daniel Mendez <silex93@gmail.com>
2023-09-17 19:50:59 -0600

    Testing do_exec

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -1,3 +1,11 @@
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/syslog.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include "systemcalls.h"
 
 /**
@@ -7,16 +15,18 @@
  *   either in invocation of the system() call, or if a non-zero return
  *   value was returned by the command issued in @param cmd.
 */
-bool do_system(const char *cmd)
-{
+bool do_system(const char *cmd) {
+    //Ensure cmd is not null
+    if (cmd == NULL) {
+        return false;
+    }
+    //Perform the system call
+    int res = system(cmd);
 
-/*
- * TODO  add your code here
- *  Call the system() function with the command set in the cmd
- *   and return a boolean true if the system() call completed with success
- *   or false() if it returned a failure
-*/
-
+    if (res != 0) {
+        printf("Error code: %d\n", errno);
+        return false;
+    }
     return true;
 }
 
@@ -34,66 +44,157 @@ bool do_system(const char *cmd)
 *   by the command issued in @param arguments with the specified arguments.
 */
 
-bool do_exec(int count, ...)
-{
+bool do_exec(int count, ...) {
+    openlog(NULL, 0, LOG_USER);
+
     va_list args;
     va_start(args, count);
-    char * command[count+1];
+    char *command[count + 1];
     int i;
-    for(i=0; i<count; i++)
-    {
+    for (i = 0; i < count; i++) {
         command[i] = va_arg(args, char *);
+
     }
     command[count] = NULL;
-    // this line is to avoid a compile warning before your implementation is complete
-    // and may be removed
-    command[count] = command[count];
 
-/*
- * TODO:
- *   Execute a system command by calling fork, execv(),
- *   and wait instead of system (see LSP page 161).
- *   Use the command[0] as the full path to the command to execute
- *   (first argument to execv), and use the remaining arguments
- *   as second argument to the execv() command.
- *
-*/
+    char *path = command[0];
+
+    //Ensure that the path is not null or empty
+    if (path == NULL || strlen(path) == 0) {
+        syslog(LOG_ERR, "AESD: Path is empty or NULL\n");
+        return false;
+    }
+
+    //Check if the path is valid - MOdified based on CHATGPT output
+    struct stat buffer;
+    if (stat(path, &buffer) != 0) {
+        syslog(LOG_ERR, "AESD: Path is invalid\n");
+        return false;
+    }
+
+    //Create new process
+    pid_t child_pid = fork();
+
+    if (child_pid == -1) {
+        syslog(LOG_ERR, "AESD: Failed to fork a child process\n");
+        return false;
+    } else if (child_pid == 0) {
+
+        // Execute the command and check if the process returns okay
+        if (execv(path, command) == -1) {
+            syslog(LOG_ERR, "AESD: Program failed to execute\n");
+            exit(EXIT_FAILURE);
+        }
+       // syslog(LOG_INFO, "AESD: Child Process Completed\n");
+        exit(EXIT_SUCCESS);
+    } else {
+        // Wait for the child process to complete
+        int status;
+       // if (waitpid(child_pid, &status, 0) == -1) {
+        if (wait(&status) == -1) {
+            syslog(LOG_ERR, "AESD: Wait failed!\n");
+            return false;
+        }
+
+        if (WIFEXITED(status)) {
+            int exit_status = WEXITSTATUS(status);
+            printf("Child process exited with status %d\n", exit_status);
+            if(exit_status != 0 ){
+                return false;
+            }
+        } else {
+            printf("Child process did not exit normally\n");
+            return false; // Command execution failed
+        }
+    }
 
     va_end(args);
-
     return true;
 }
-
 /**
 * @param outputfile - The full path to the file to write with command output.
 *   This file will be closed at completion of the function call.
 * All other parameters, see do_exec above
 */
-bool do_exec_redirect(const char *outputfile, int count, ...)
-{
+//Reference used: https://stackoverflow.com/questions/13784269/redirection-inside-call-to-execvp-not-working/13784315#13784315
+bool do_exec_redirect(const char *outputfile, int count, ...) {
+    openlog(NULL, 0, LOG_USER);
+    int fd ;
     va_list args;
     va_start(args, count);
-    char * command[count+1];
+    char *command[count + 1];
     int i;
-    for(i=0; i<count; i++)
-    {
+    for (i = 0; i < count; i++) {
         command[i] = va_arg(args, char *);
     }
     command[count] = NULL;
-    // this line is to avoid a compile warning before your implementation is complete
-    // and may be removed
-    command[count] = command[count];
-
-
-/*
- * TODO
- *   Call execv, but first using https://stackoverflow.com/a/13784315/1446624 as a refernce,
- *   redirect standard out to a file specified by outputfile.
- *   The rest of the behaviour is same as do_exec()
- *
-*/
 
     va_end(args);
+    char *path = command[0];
 
+    //Ensure that the paths are not null or empty
+    if (path == NULL || strlen(path) == 0 || outputfile == NULL || strlen(outputfile) == 0) {
+        syslog(LOG_ERR, "AESD: Paths are empty or NULL\n");
+        return false;
+    }
+
+    //Check if the paths are valid - Modified based on CHATGPT output
+    struct stat buffer;
+    if (stat(path, &buffer) != 0 || stat(outputfile, &buffer) != 0) {
+        syslog(LOG_ERR, "AESD: Path is invalid\n");
+        return false;
+    }
+
+    //Check that the file was opened correctly
+    fd = open("proc_std_out.txt", O_WRONLY|O_TRUNC|O_CREAT, 0644);
+    if (fd < 0) {
+        syslog(LOG_ERR, "AESD: Failed to open redirection file \n");
+        return false;
+    }
+    //Create new process
+    pid_t child_pid = fork();
+
+    if (child_pid == -1) {
+        syslog(LOG_ERR, "AESD: Failed to fork a child process\n");
+        return false;
+    } else if (child_pid == 0) {
+
+        //Duplicate the file descriptor
+        if (dup2(fd, 1) < 0) {
+            syslog(LOG_ERR, "AESD: Failed to duplicate file descriptor\n");
+            return false;
+        }
+        //Close the original fd
+        close(fd);
+
+        // Execute the command and check if the process returns okay
+        if (execv(path, command) == -1) {
+            syslog(LOG_ERR, "AESD: Program failed to execute\n");
+            exit(EXIT_FAILURE);
+        }
+
+      //  exit(EXIT_SUCCESS);
+    } else {
+        // Wait for the child process to complete
+        int status;
+        if (waitpid(child_pid,&status,0) == -1) {
+            syslog(LOG_ERR, "AESD: Wait failed!\n");
+            return false;
+        }
+
+        if (WIFEXITED(status)) {
+            int exit_status = WEXITSTATUS(status);
+            printf("Child process exited with status %d\n", exit_status);
+            if(exit_status != 0 ){
+                return false;
+            }
+        } else {
+            printf("Child process did not exit normally\n");
+            return false; // Command execution failed
+        }
+        close(fd);
+    }
     return true;
+
+
 }
